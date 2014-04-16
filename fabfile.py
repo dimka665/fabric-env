69e9459203285bbd299b0f2ad6aab2d729e283ea
dimka665 <dimka665@gmail.com>
2014-04-16 04:22:51 +0400

    Global refactoring: Init is done

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,3 @@
 # coding=utf8
 
-from fabric_env.fabfile import *
+from fuzzy_fabric.fabfile import *
--- setup.py
+++ setup.py
@@ -1,54 +1,32 @@
-from setuptools import setup
-import os
+# coding=utf8
 
-name = 'fabric-env'
-package = 'fabric_env'
-version = '0.5.4'
-
-url = 'https://github.com/dimka665/fabric_env'
-author = 'Dmitry Voronin'
-author_email = 'dimka665@gmail.com'
-license_ = 'BSD'
-
-description = 'Accessory classes and functions for Fabric'
-long_description = open('README.rst').read()
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
+from setuptools import setup, find_packages
 
 setup(
-    name=name,
-    version=version,
-    url=url,
-    license=license_,
-    description=description,
-    long_description=long_description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
+    name='fuzzy-fabric',
+    version='0.6',
+
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
+
+    url='https://github.com/dimka665/fuzzy-fabric',
+    description='Fuzzy Functions For Fabric',
+
+    packages=find_packages(),
+    install_requires=[
+        'Fabric',
+        'virtualenv',
+        'virtualenvwrapper',
+    ],
+
+    license='MIT License',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='fuzzy functions for fabric',
 )
-
-
